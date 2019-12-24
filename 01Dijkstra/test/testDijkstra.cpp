42c9c88bfcace65dd0066b72ef5f6018d8402e43
David Davó <david@ddavo.me>
2019-12-24 21:14:01 +0100

    Moved some tests

---
+++ 01Dijkstra/test/testDijkstra.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "dijkstra.h"
+
+using namespace std;
+
+class TestDijkstra : public :: testing::Test {
+protected:
+    LabeledWeaightedGraph<unsigned, int> _g1;
+
+    void SetUp() override {
+        _g1.in
+    }
+}
