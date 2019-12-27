ed056b3978f27ccc780eb5b0b2791bd37c536bff
David Davó <david@ddavo.me>
2019-12-27 16:11:09 +0100

    Al menos ya compila

--- 01Dijkstra/src/dijkstra.h
+++ 01Dijkstra/src/dijkstra.h
@@ -1,39 +1,98 @@
 #pragma once
-
 #include <vector>
+#include "skewheap.h"
 
-class Vertex;
+/** Type declarations */
 
-class Edge {
-	Vertex * _from;
-	Vertex * _to;
+typedef unsigned edge_descriptor;
+typedef unsigned vertex_descriptor;
+
+template <class W> 
+struct Edge {
+    edge_descriptor ed;
+    vertex_descriptor to;
+    vertex_descriptor from;
+    W weight;
+    Edge (edge_descriptor ed, vertex_descriptor from, vertex_descriptor to, W weight = 1) :
+        ed(ed), to(to), from(from), weight(weight) {}
 };
 
 template <class W>
-class WeightedEdge : Edge {
-private:
-	W _weight;
+struct Vertex {
+    vertex_descriptor vd;
+    std::string label;
+    std::vector<Edge<W>> edges;
+    Vertex (vertex_descriptor vd, std::string label) : vd(vd), label(label) {}
 };
 
-class Vertex {
-	std::vector<Edge> _edges;
-};
+void hw() {
+    printf("Hello World\n");
+}
 
-template <class L>
-class LabeledVertex : Vertex {
-	L _label;
-};
+template <class W> using Graph = std::vector<Vertex<W>>;
 
-/**
- * Graph represented using a adjacency list
- * Each Vertex can have a label, each edge can have
- * a weight
+/*
+ *FUNCTION DECLARATIONS
  */
-template <class L, class W>
-class LabeledWeightedGraph {
-private:
-	std::vector<LabeledVertex<L>> _adjList;
-};
 
-template <class L, class W>
-std::vector<std::pair<W, LabeledVertex<L>>> dijkstra(LabeledWeightedGraph<L, W> g, Vertex & source);
+template <class W>
+vertex_descriptor add_vertex(Graph<W> & g, std::string label) {   
+    if (g.size() == 0) g.push_back(Vertex<W>(0, "null"));
+    g.push_back(Vertex<W>(g.size(), label));
+    return g.size() - 1;
+}
+
+template <class W>
+edge_descriptor add_edge(Vertex<W> & from, const Vertex<W> & to, W weight = 1) {
+    if (from.vd == 0 || to.vd == 0) throw std::range_error("Can't connect to null");
+    auto edges = from.edges;
+    edges.push_back(Edge<W>(edges.size(), from.vd, to.vd, weight));
+    return edges.size() - 1;
+}
+
+template <class W>
+edge_descriptor add_edge(Graph<W> & g, vertex_descriptor from, vertex_descriptor to, W weight = 1) {
+    return add_edge(g[from], g[to], weight);
+}
+
+template <class W>
+std::vector<std::pair<W, vertex_descriptor>> dijkstra(const Graph<W> & g, const Vertex<W> & source) {
+    return dijkstra(g, source.vd);
+}
+
+template <class W>
+std::vector<std::pair<W, vertex_descriptor>> dijkstra(const Graph<W> & g, vertex_descriptor vd) {
+    if (vd == 0) throw std::range_error("vd can't be 0 (NULL)");
+    std::vector<W> distances(g.size());
+    std::vector<vertex_descriptor> path(g.size());
+
+    SkewHeap<W, vertex_descriptor> pq;
+    // auto idk = pq.getMin();
+
+    for (vertex_descriptor i = 1; i < g.size(); ++i) {
+        if (i == vd) distances[i] = 0;
+        else distances[i] = std::numeric_limits<W>::max();
+
+        path[i] = 0;
+        // nodes[i] = pq.insert(distances[i], i);
+    }
+
+    vertex_descriptor currentV;
+    while(!pq.empty()) {
+        assert(pq.getMin()->getKey() == distances[pq.getMin()->getVal()]);
+        currentV = pq.getMin()->getVal();
+        pq.deleteMin();
+
+        for (Edge<W> e : g[currentV].edges) {
+            W tmpdst;
+
+            tmpdst = distances[currentV] + e.weight;
+            if (tmpdst < distances[e.to]) {
+                distances[e.to] = tmpdst;
+                path[e.to] = currentV;
+                /* We need to save the pointer to that node */
+                // pq.decreaseKey(currentN, tmpdst);
+            }
+        }
+    }
+}
--- 01Dijkstra/test/testDijkstra.cpp
+++ 01Dijkstra/test/testDijkstra.cpp
@@ -5,9 +5,31 @@ using namespace std;
 
 class TestDijkstra : public :: testing::Test {
 protected:
-    LabeledWeaightedGraph<unsigned, int> _g1;
+    typedef vertex_descriptor vd;
+    typedef edge_descriptor ed;
+
+    Graph<unsigned> _g0;
+    Graph<unsigned> _g1;
 
     void SetUp() override {
-        _g1.in
+        add_vertex(_g1, "v1");
+        add_vertex(_g1, "v2");
+        add_edge(_g1, 1, 2);
     }
+};
+
+TEST_F(TestDijkstra, Invalid) {
+    EXPECT_THROW(dijkstra(_g0, 0), range_error);
+}
+
+TEST_F(TestDijkstra, TwoNodes) {
+    auto ret = dijkstra(_g1, _g1[1]); 
+    for (int i = 0; i < ret.size(); ++i) {
+        printf("W: %d, Path: %d\n", ret[i].first, ret[i].second);
+    }
+}
+
+int main(int argc, char * argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
